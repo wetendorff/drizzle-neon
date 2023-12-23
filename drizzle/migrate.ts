8862a3d9008a5beacb9b9d71ac012aa9b1832f56
Lars Wetendorff Nielsen <larswn@me.com>
2023-12-23 15:26:36 +0100

    fix: use database_url in migrate

--- drizzle/migrate.ts
+++ drizzle/migrate.ts
@@ -1,10 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
-import * as schema from "~/drizzle/schema";
+// import * as schema from "~/drizzle/schema";
 
-const connection = postgres(process.env.NUXT_CONNECTION_STRING!, { max: 1 });
-const db = drizzle(connection, { schema });
+const connection = postgres(process.env.DATABASE_URL!, { max: 1 });
+const db = drizzle(connection);
 
 await migrate(db, { migrationsFolder: "drizzle" });
 await connection.end();
