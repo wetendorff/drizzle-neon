760321429be8271ec26ec78cd28c06fc2a0aabf0
Lars Wetendorff Nielsen <larswn@me.com>
2023-12-26 17:55:58 +0100

    ci: output database url

--- drizzle/migrate.ts
+++ drizzle/migrate.ts
@@ -3,6 +3,8 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 // import * as schema from "~/drizzle/schema";
 
+console.log("Migrating database...", process.env.DATABASE_URL);
+
 const connection = postgres(process.env.DATABASE_URL!, {
   max: 1,
   ssl: "require",
